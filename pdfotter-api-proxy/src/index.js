ae01223cd2c177f221990604c5f48ecd4e70b473
Roy <24995826+waltermarbel@users.noreply.github.com>
2025-11-08 22:03:45 -0500

    Apply suggestion from @codacy-production[bot]
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- pdfotter-api-proxy/src/index.js
+++ pdfotter-api-proxy/src/index.js
@@ -39,7 +39,7 @@ export default {
     requestHeaders.delete('X-Worker-Key');
 
     try {
-      const response = await fetch(targetUrl, {
+      const response = await fetch(targetUrl.startsWith(PDF_OTTER_BASE_URL) ? targetUrl : new Response(null, { status: 400 }), {
         method: request.method,
         headers: requestHeaders,
         body: request.body,
